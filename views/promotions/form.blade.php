63b3df41a903f13325e34a96200cf862981e2680
idominguez <israel.dominguez.garcia@gmail.com>
2018-12-05 16:02:08 +0100

    delete previous codes and moments

--- views/promotions/form.blade.php
+++ views/promotions/form.blade.php
@@ -118,6 +118,16 @@
                 <i class="form-group__bar"></i>
             </div>
         </div>
+        <div class="col-xs-12 col-md-6">
+            <div class="form-group">
+                <label>Remove Previous Moments</label>
+                <br>
+                <div class="toggle-switch">
+                    <input type="checkbox" name="remove_prev" class="toggle-switch__checkbox">
+                    <i class="toggle-switch__helper"></i>
+                </div>
+            </div>
+        </div>
         @if(!empty($promotion))
         <div class="col-xs-12 col-md-6">
             Total Moments: {{ count($promotion->moment) }}
@@ -137,6 +147,16 @@
                 <label>Import File (csv) <a href="{{route('download-sample', ['file' => 'pincodes_sample'])}}" target="_blank">download sample</a></label>
                 <input type="file" class="form-control" name="pincodes_file" id="pincodes_file" accept=".csv">
                 <i class="form-group__bar"></i>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6">
+            <div class="form-group">
+                <label>Remove Previous Moments</label>
+                <br>
+                <div class="toggle-switch">
+                    <input type="checkbox" name="remove_prev" class="toggle-switch__checkbox">
+                    <i class="toggle-switch__helper"></i>
+                </div>
             </div>
         </div>
         @if(!empty($promotion))
