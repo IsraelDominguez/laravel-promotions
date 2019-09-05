6649898f1f715ccfc7cc26da715c0cd4e5cc7150
idominguez <israel.dominguez.garcia@gmail.com>
2019-09-05 15:42:51 +0200

    changes for laravel 6

--- database/migrations/2018_12_13_000002_add_win_codes.php
+++ database/migrations/2018_12_13_000002_add_win_codes.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddLegalLinkCampaignTable extends Migration
+class AddWinCodes extends Migration
 {
     /**
      * Run the migrations.
