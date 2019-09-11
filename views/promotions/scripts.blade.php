cbed0aa3d81afa396696d64dd59536dccaeca58c
idominguez <israel.dominguez.garcia@gmail.com>
2019-09-11 10:41:57 +0200

    not load templates js

--- views/promotions/scripts.blade.php
+++ views/promotions/scripts.blade.php
@@ -1,68 +1,89 @@
 <script>
-    var schema_left = {
-        "properties": {
-            "promo_title": {
-                "type": "string",
-                "title": "Title"
-            },
-            "promo_text": {
-                "type": "string",
-                "title": "Text"
-            },
-            "promo_image": {
-                "type": "string",
-                "format": "uri"
-            }
-        }
-    };
-
-    var schema_right = {
-        "properties": {
-            "promo_title": {
-                "type": "string",
-                "title": "Title"
-            },
-            "promo_text": {
-                "type": "string",
-                "title": "Text"
-            },
-            "promo_image": {
-                "type": "string"
-            }
-        }
-    };
-
-    var postRenderCallback = function(control) {
-        var id = "img-"+$(control.domEl).attr("id");
-        $('#'+$(control.domEl).attr("id")+' #promo_image').attr('name', "promo_image_"+$(control.domEl).attr("id"));
-        control.getFieldEl().append("<div id='"+id+"' style='display:none'><table><tr><td nowrap='nowrap' class='imagePreview' style='width: 220px'> </td></tr></table></div>");
-        if ((control.data.promo_image != undefined) && (control.data.promo_image != '')) {
-            var img = $("#"+id+" .imagePreview").html("").append("<img style='max-width: 200px; max-height: 200px' src='<?php echo Storage::disk('public')->url('/') ?>"+control.data.promo_image+"'>");
-            $("#"+id).css({
-                "display": "block"
-            });
-        }
-    };
-    var options = {
-        "fields": {
-            "promo_text": {
-                "type": "tinymce"
-            },
-            "promo_image": {
-                "type": "file",
-                "id": "promo_image",
-                "fieldClass": "input-file",
-                "selectionHandler": function(files, data) {
-                    var id = "img-"+$(this.parent.domEl).attr("id");
-                    $("#"+id+" .imagePreview").html("").append("<img style='max-width: 200px; max-height: 200px' src='" + data[0] + "'>");
-                    $("#"+id).css({
-                        "display": "block"
-                    });
-
+    @if (config('promotion.front_templates_enabled'))
+        var schema_left = {
+            "properties": {
+                "promo_title": {
+                    "type": "string",
+                    "title": "Title"
+                },
+                "promo_text": {
+                    "type": "string",
+                    "title": "Text"
+                },
+                "promo_image": {
+                    "type": "string",
+                    "format": "uri"
                 }
             }
-        }
-    };
+        };
+
+        var schema_right = {
+            "properties": {
+                "promo_title": {
+                    "type": "string",
+                    "title": "Title"
+                },
+                "promo_text": {
+                    "type": "string",
+                    "title": "Text"
+                },
+                "promo_image": {
+                    "type": "string"
+                }
+            }
+        };
+
+        var postRenderCallback = function(control) {
+            var id = "img-"+$(control.domEl).attr("id");
+            $('#'+$(control.domEl).attr("id")+' #promo_image').attr('name', "promo_image_"+$(control.domEl).attr("id"));
+            control.getFieldEl().append("<div id='"+id+"' style='display:none'><table><tr><td nowrap='nowrap' class='imagePreview' style='width: 220px'> </td></tr></table></div>");
+            if ((control.data.promo_image != undefined) && (control.data.promo_image != '')) {
+                var img = $("#"+id+" .imagePreview").html("").append("<img style='max-width: 200px; max-height: 200px' src='<?php echo Storage::disk('public')->url('/') ?>"+control.data.promo_image+"'>");
+                $("#"+id).css({
+                    "display": "block"
+                });
+            }
+        };
+        var options = {
+            "fields": {
+                "promo_text": {
+                    "type": "tinymce"
+                },
+                "promo_image": {
+                    "type": "file",
+                    "id": "promo_image",
+                    "fieldClass": "input-file",
+                    "selectionHandler": function(files, data) {
+                        var id = "img-"+$(this.parent.domEl).attr("id");
+                        $("#"+id+" .imagePreview").html("").append("<img style='max-width: 200px; max-height: 200px' src='" + data[0] + "'>");
+                        $("#"+id).css({
+                            "display": "block"
+                        });
+
+                    }
+                }
+            }
+        };
+
+        Alpaca.Fields.TinyMCEField = Alpaca.Fields.TextAreaField.extend({
+            afterRenderControl: function(model, callback) {
+                var n = this;
+                this.base(model, function() {
+                    !n.isDisplayOnly() && n.control && "undefined" != typeof tinyMCE && n.on("ready", function() {
+                        if (!n.editor) {
+                            var t = $(n.control)[0].id;
+                            tinyMCE.init({
+                                selector: "#" + t,
+                                toolbar: n.options.toolbar,
+                                plugins: ["link"]
+                            })
+                        }
+                    }), callback()
+                })
+            },
+        });
+        Alpaca.registerFieldClass("tinymce", Alpaca.Fields.TinyMCEField);
+    @endif
 
     var entrypoint_selected = '{{$promotion->entrypoint_id ?? ''}}';
 
@@ -91,13 +112,13 @@
                 initial_template = $("#initial_page_template").val();
                 result_template = $("#result_page_template").val();
 
-            if (initial_template != '') {
-                $('#initial_page_data').val(JSON.stringify($("#initial_page_template_" + initial_template).alpaca("get").getValue()));
-            }
+                if (initial_template != '') {
+                    $('#initial_page_data').val(JSON.stringify($("#initial_page_template_" + initial_template).alpaca("get").getValue()));
+                }
 
-            if (result_template != '') {
-                $('#result_page_data').val(JSON.stringify($("#result_page_template_" + result_template).alpaca("get").getValue()));
-            }
+                if (result_template != '') {
+                    $('#result_page_data').val(JSON.stringify($("#result_page_template_" + result_template).alpaca("get").getValue()));
+                }
             @endif
 
             $("#form").submit();
