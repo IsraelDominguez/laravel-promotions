8192cff11298c39651e4f4cb8a6f9738b1b07e1c
idominguez <israel.dominguez.garcia@gmail.com>
2019-11-18 17:53:31 +0100

    Campaign only for Super Admin

--- views/partials/promotion_menu.blade.php
+++ views/partials/promotion_menu.blade.php
@@ -2,7 +2,7 @@
 <li class="navigation__sub {{ (($section=='campaign')||($section=='promotion')) ? 'navigation__sub--active' : '' }}">
     <a href=""><i class="zmdi zmdi-card-giftcard"></i> Promotions</a>
     <ul>
-        @hasanyrole('SuperAdmin|Admin')
+        @role('SuperAdmin')
         <li class="{{ ($section=='campaign') ? 'navigation__active' : '' }}"><a href="{{ route('campaigns.home') }}">Campaigns</a></li>
         @endrole
         <li class="{{ ($section=='promotion') ? 'navigation__active' : '' }}"><a href="{{ route('promotions.home') }}">Promotions</a></li>
